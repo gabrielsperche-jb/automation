b884dbc6f0aaa9f19d16ab8650183797d04046e7
Gabriel Gonçalves <97049708+gabrielgoncalves-jb@users.noreply.github.com>
2023-02-27 17:37:09 -0300

    Action test

--- auxiliar-connector/src/main/java/org/example/Main.java
+++ auxiliar-connector/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Axiliary Module!");
     int num = 9;
-    int hnum = 9;
+    int hnum = 79;
     int hfnum = 10;
     String test = "";
   }
