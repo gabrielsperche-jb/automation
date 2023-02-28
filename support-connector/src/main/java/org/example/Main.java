102890ed8341de0b9b3457f9afb6fc766aa09aec
Gabriel Gonçalves <97049708+gabrielgoncalves-jb@users.noreply.github.com>
2023-02-28 11:57:24 -0300

    Test build more than one module

--- support-connector/src/main/java/org/example/Main.java
+++ support-connector/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Support Module!");
-    int e = 9244;
+    int e = 94;
     int et = 94;
   }
 }
