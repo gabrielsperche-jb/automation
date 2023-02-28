1e046d9fcc7e1b12dcc5cd049ca401e086b73e1d
Gabriel Gonçalves <97049708+gabrielgoncalves-jb@users.noreply.github.com>
2023-02-28 11:41:40 -0300

    Test build more than one module

--- auxiliar-connector/src/main/java/org/example/Main.java
+++ auxiliar-connector/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Axiliary Module!");
+    System.out.println("Auxiliary Module!");
     int num = 9;
     int hnum = 99;
     int hfnum = 15420;
