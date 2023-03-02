9c528075374b28b1b0c503a6df7a29f3be46a2e4
Gabriel Gonçalves <97049708+gabrielgoncalves-jb@users.noreply.github.com>
2023-03-02 16:42:27 -0300

    Automation Flow (Until FTP) - Two Modules Changed

--- auxiliar-connector/src/main/java/org/example/Main.java
+++ auxiliar-connector/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Auxiliary Module!");
     int num = 9;
-    int hnum = 834;
+    int hnum = 4;
     int hfnum = 140;
     String test = "fk";
   }
