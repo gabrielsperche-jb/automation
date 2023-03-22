425724a3e08b9590d71705ffbfc8a08131258e93
Gabriel Gonçalves <97049708+gabrielgoncalves-jb@users.noreply.github.com>
2023-03-22 09:35:19 -0300

    Testing new resilience

--- connectors-common/src/main/java/org/example/Main.java
+++ connectors-common/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hellotttr  df  ggggwyorld!");
+    System.out.println("Hellotttrc  df  ggggwyorld!");
   }
 }
