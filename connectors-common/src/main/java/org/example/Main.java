bacc468ca347837fcc3464e1e2bcdb7b9aaa5490
Gabriel Gonçalves <97049708+gabrielgoncalves-jb@users.noreply.github.com>
2023-03-22 11:31:06 -0300

    Listing common dependents

--- connectors-common/src/main/java/org/example/Main.java
+++ connectors-common/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello          ggwyorld!");
+    System.out.println("Hello           ggwyorld!");
   }
 }
